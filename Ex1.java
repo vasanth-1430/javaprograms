d680013aa7a39e1c69a2bdf410d37c337ee61b4f
vasanth R <vasanthrajavel480@gmail.com>
2025-02-04 14:35:50 +0530

    first commit

---
+++ Ex1.java
@@ -0,0 +1,14 @@
+//Arithmetic operations
+public class Ex1 {
+    public static void main(String args[])
+    {
+        //Data members
+        int a=10;
+        int b=20;
+        System.out.println("Addition of 2No's:"+(a+b));
+        System.out.println("subtraction of 2No's:"+(a-b));
+        System.out.println("MUltiplication of 2No's:"+(a*b));
+        System.out.println("division of 2No's:"+(a/b));
+        System.out.println("MODulus of 2No's:"+(a%b));
+    }
+}
